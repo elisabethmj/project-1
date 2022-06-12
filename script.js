2adbac6264208307d736379272617bafdcd9922d
Elise James <elisabethmariajames@gmail.com>
2022-06-12 23:38:45 +1000

    HTML: Added 6th attempt input field. JS: Figured out how to put individual input fields into an array.

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
-const guess = for (const input of document.querySelectorAll('.currentattempt input')) { 
-    console.log(input.value)
-}
-
 
 
 // //Variables for secret word and user input and submit button
 // //Compare function when submitted with button
-// const secretWord = 'hello';
-// const button = document.querySelector('#submit');
+const secretWord = 'hello';
+const button = document.querySelector('#submit');
 
-// button.addEventListener('click', compare);
+button.addEventListener('click', compare);
 
 
-// function compare() {
-//     //User input variable has to be in function so it's not grabbing value as page is loaded.
-//     const userInput = document.querySelector('#userinput').value;
-//     console.log('The user input is ' + userInput + '. And the secret word is ' + secretWord);
-   
-//     console.log(document.querySelectorAll('.tile'));
+function compare() {
+    //User input variable has to be in function so it's not grabbing value as page is loaded.
+    const userInput = document.querySelector('#userinput').value;
+    console.log('The user input is ' + userInput + '. And the secret word is ' + secretWord);
+  
 
-//     //Compare the secret word and user input - letter by letter - 
-//     //1. User input and secret world will be a string. Split both strings into array of letters.
-//     const secretWordArray = secretWord.split('');
-//     const userInputArray = userInput.split('');
-//     console.log(secretWordArray);
-//     console.log(userInputArray);
+    //Compare the secret word and user input - letter by letter - 
+    //1. User input and secret world will be a string. Split both strings into array of letters.
+    const secretWordArray = secretWord.split('');
+    // console.log(secretWordArray);
     
-//     //if userInput[index] = secretWordArray[index] - green tile
-//     //if 
+    const userInputArray = [];
 
-//     // if (userInputArray[0] === secretWordArray[0]) {
-//     //     console.log('0 green');
-//     // } else if (secretWordArray.includes(userInputArray[0])) {
-//     //     console.log('0 yellow');
-//     // } else if (!secretWordArray.includes(userInputArray[0])) {
-//     //     console.log('0 black');
-//     // }
+    for (const input of document.querySelectorAll('.currentattempt input')) { 
+    // console.log(input.value) 
+        userInputArray.push((input.value).toLowerCase());
+        console.log(userInputArray)
+    }
 
-//     // if (userInputArray[1] === secretWordArray[1]) {
-//     //     console.log('1 green');
-//     // } else if (secretWordArray.includes(userInputArray[1])) {
-//     //     console.log('1 yellow');
-//     // } else if (!secretWordArray.includes(userInputArray[1])) {
-//     //     console.log('1 black');
-//     // }
+    if (userInputArray[0] === secretWordArray[0]) {
+        console.log('0 green');
+    } else if (secretWordArray.includes(userInputArray[0])) {
+        console.log('0 yellow');
+    } else if (!secretWordArray.includes(userInputArray[0])) {
+        console.log('0 black');
+    }
 
-//     // if (userInputArray[2] === secretWordArray[2]) {
-//     //     console.log('2 green');
-//     // } else if (secretWordArray.includes(userInputArray[2])) {
-//     //     console.log('2 yellow');
-//     // } else if (!secretWordArray.includes(userInputArray[2])) {
-//     //     console.log('2 black');
-//     // }
+    if (userInputArray[1] === secretWordArray[1]) {
+        console.log('1 green');
+    } else if (secretWordArray.includes(userInputArray[1])) {
+        console.log('1 yellow');
+    } else if (!secretWordArray.includes(userInputArray[1])) {
+        console.log('1 black');
+    }
 
-//     // if (userInputArray[3] === secretWordArray[3]) {
-//     //     console.log('3 green');
-//     // } else if (secretWordArray.includes(userInputArray[3])) {
-//     //     console.log('3 yellow');
-//     // } else if (!secretWordArray.includes(userInputArray[3])) {
-//     //     console.log('3 black');
-//     // }
+    if (userInputArray[2] === secretWordArray[2]) {
+        console.log('2 green');
+    } else if (secretWordArray.includes(userInputArray[2])) {
+        console.log('2 yellow');
+    } else if (!secretWordArray.includes(userInputArray[2])) {
+        console.log('2 black');
+    }
 
-//     // if (userInputArray[4] === secretWordArray[4]) {
-//     //     console.log('4 green');
-//     // } else if (secretWordArray.includes(userInputArray[4])) {
-//     //     console.log('4 yellow');
-//     // } else if (!secretWordArray.includes(userInputArray[4])) {
-//     //     console.log('4 black');
-//     // }
+    if (userInputArray[3] === secretWordArray[3]) {
+        console.log('3 green');
+    } else if (secretWordArray.includes(userInputArray[3])) {
+        console.log('3 yellow');
+    } else if (!secretWordArray.includes(userInputArray[3])) {
+        console.log('3 black');
+    }
 
-// }
+    if (userInputArray[4] === secretWordArray[4]) {
+        console.log('4 green');
+    } else if (secretWordArray.includes(userInputArray[4])) {
+        console.log('4 yellow');
+    } else if (!secretWordArray.includes(userInputArray[4])) {
+        console.log('4 black');
+    }
+
+}
     
 
     
