34d55a342627597ad636db132a519a32fede4185
Elise James <elisabethmariajames@gmail.com>
2022-06-13 17:53:48 +1000

    HTML: Added some additional features. CSS: Tidied up some colors etc. JS: Added tile changing colors as opposed to just console.log.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #Elise's Wordle
+
+##Steps
+
+1. HTML/CSS - Structure & styling
+    i. 6 user guesses
+    ii. Enter/Submit guess
+    iii. Disable later guesses until first attempt done
+    
+2. Wire up event to render letter per attempt 
+    i. Capture 5 letters
+
+3. Render hints to the user
+
+4. Branch based on result of step 3
+
+My steps:
+
--- script.js
+++ script.js
@@ -3,77 +3,94 @@
 // //Variables for secret word and user input and submit button
 // //Compare function when submitted with button
 const secretWord = 'hello';
-const button = document.querySelector('#submit');
+const button = document.querySelector('#submit-guess');
 
 button.addEventListener('click', compare);
 
 
 function compare() {
-    //User input variable has to be in function so it's not grabbing value as page is loaded.
-    const userInput = document.querySelector('#userinput').value;
-    console.log('The user input is ' + userInput + '. And the secret word is ' + secretWord);
-  
-
     //Compare the secret word and user input - letter by letter - 
     //1. User input and secret world will be a string. Split both strings into array of letters.
     const secretWordArray = secretWord.split('');
-    // console.log(secretWordArray);
     
+    //User input variable has to be in function so it's not grabbing value as page is loaded.
     const userInputArray = [];
 
+    //Loop to push separate letters into array to be compared to secret word
     for (const input of document.querySelectorAll('.currentattempt input')) { 
     // console.log(input.value) 
         userInputArray.push((input.value).toLowerCase());
-        console.log(userInputArray)
+        //console.log(userInputArray)
+
+        //As it iterates through array, check that letter against secret word and color tile accordingly?
     }
 
+    //Massive code below to compare user input to secret word and give hints for tiles! - srsly needs some optimisation. Maybe put below code within loop that is currently pushing guess into an array?
+    const firsttile = document.querySelector('.currentattempt > #firstletter');
     if (userInputArray[0] === secretWordArray[0]) {
-        console.log('0 green');
+        //console.log('0 green');
+        firsttile.style.backgroundColor = 'green';
     } else if (secretWordArray.includes(userInputArray[0])) {
-        console.log('0 yellow');
+        //console.log('0 yellow');
+        firsttile.style.backgroundColor = 'yellow';
     } else if (!secretWordArray.includes(userInputArray[0])) {
-        console.log('0 black');
+        //console.log('0 black');
+        firsttile.style.backgroundColor = 'grey';
     }
 
+    const secondtile = document.querySelector('.currentattempt > #secondletter');
     if (userInputArray[1] === secretWordArray[1]) {
-        console.log('1 green');
+        //console.log('1 green');
+        secondtile.style.backgroundColor = 'green';
     } else if (secretWordArray.includes(userInputArray[1])) {
-        console.log('1 yellow');
+        //console.log('1 yellow');
+        secondtile.style.backgroundColor = 'yellow';
     } else if (!secretWordArray.includes(userInputArray[1])) {
-        console.log('1 black');
+        //console.log('1 black');
+        secondtile.style.backgroundColor = 'grey';
     }
 
+    const thirdtile = document.querySelector('.currentattempt > #thirdletter');
     if (userInputArray[2] === secretWordArray[2]) {
-        console.log('2 green');
+        //console.log('2 green');
+        thirdtile.style.backgroundColor = 'green';
     } else if (secretWordArray.includes(userInputArray[2])) {
-        console.log('2 yellow');
+        //console.log('2 yellow');
+        thirdtile.style.backgroundColor = 'yellow';
     } else if (!secretWordArray.includes(userInputArray[2])) {
-        console.log('2 black');
+        //console.log('2 black');
+        thirdtile.style.backgroundColor = 'grey';
     }
 
+    const fourthtile = document.querySelector('.currentattempt > #fourthletter');
     if (userInputArray[3] === secretWordArray[3]) {
-        console.log('3 green');
+        //console.log('3 green');
+        fourthtile.style.backgroundColor = 'green';
     } else if (secretWordArray.includes(userInputArray[3])) {
-        console.log('3 yellow');
+        //console.log('3 yellow');
+        fourthtile.style.backgroundColor = 'yellow';
     } else if (!secretWordArray.includes(userInputArray[3])) {
-        console.log('3 black');
+        //console.log('3 black');
+        fourthtile.style.backgroundColor = 'grey';
+    }
+    const fifthtile = document.querySelector('.currentattempt > #fifthletter');
+    if (userInputArray[4] === secretWordArray[4]) {
+        //console.log('4 green');
+        fifthtile.style.backgroundColor = 'green';
+    } else if (secretWordArray.includes(userInputArray[4])) {
+        //console.log('4 yellow');
+        fifthtile.style.backgroundColor = 'yellow';
+    } else if (!secretWordArray.includes(userInputArray[4])) {
+        //console.log('4 black');
+        fifthtile.style.backgroundColor = 'grey';
     }
 
-    if (userInputArray[4] === secretWordArray[4]) {
-        console.log('4 green');
-    } else if (secretWordArray.includes(userInputArray[4])) {
-        console.log('4 yellow');
-    } else if (!secretWordArray.includes(userInputArray[4])) {
-        console.log('4 black');
-    }
+    //Needs code that puts current attempt class to next line of input
 
 }
-    
+ 
+//Loop through inputs 
 
-    
-//      /*
-    
-//     */
     
 
 
